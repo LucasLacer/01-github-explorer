605811049a8cd4e5e12002712b6c91cca2919b09
LucasLacer <lucaslacerdapereira@gmail.com>
2021-07-11 19:01:04 -0300

    setup project

---
+++ dist/bundle.js
@@ -0,0 +1,6 @@
+function App() {
+  const user = {
+    name: 'lucas'
+  };
+  console.log(user.address?.street);
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+function App() {
+    const user = {
+        name: 'lucas',
+    }
+
+    console.log(user.address?.street)
+}
